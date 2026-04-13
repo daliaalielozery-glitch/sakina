8f9cdcd59d37c5815a6a267e454ff4baf7e7d6c4
dalia <daliaalielozery@gmail.com>
2026-04-13 21:59:13 +0200

    adding screens

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryBrown = Color(0xff2C2005);
+  static const Color primaryBeig = Color(0xffEADEC6);
+  static const Color themeColor = Color(0xffF2ECDE);
+}
---
+++ lib/sakina_app.dart
@@ -0,0 +1,22 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:sakina/features/onboarding/ui/onboarding_screens.dart';
+class SakinaApp extends StatelessWidget {
+  const SakinaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(390, 884),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+        home: OnboardingScreens() ,
+      ),
+    );
+  }
+}
